af97999f08dc2e131513c44c88bdb5c70524ab9b
youssef.el-mkhantar <youssef.el-mkhantar@i-carre.net>
2022-02-14 16:51:42 +0100

    merge conflict

--- test/db/dbconnect.js
+++
@@ -1,6 +0,0 @@
-var MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
-
-module.exports = async () => {
-    const mongod = await MongoMemoryServer.create();
-    console.log(mongod.getUri());
-}
