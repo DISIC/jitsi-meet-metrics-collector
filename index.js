15da76160cc7ad65585b768a58cf92733bb2a669
youssef.el-mkhantar <youssef.el-mkhantar@i-carre.net>
2022-03-23 15:11:59 +0100

    correcting metrics bug

--- index.js
+++ index.js
@@ -14,19 +14,47 @@ var wrapper = function (config){
     return async function jmmc (req, res, next){
 
         if(req.url === "/getIp" && req.method === 'GET'){
-            return res.status(200).send({ip: req.ip});
+            return res.status(200).send( {ip: req.ip} );
         }
 
         if( req.url === "/push" && req.method === 'POST'){
             try {
                 if(req.body.m) {
                     req.body.m.ts = Math.floor(Date.now() / 1000); ;  // appending the timestamp to the m (metrics) variable of the object received
-                    const validation = schema_validator.validate(req.body); // validation receives an object that has value and error (in case of an error)
+                    const validation = schema_validator.validate(req.body, { abortEarly: false }); // validation receives an object that has value and error (in case of an error)
                     let formated_data = validation.value;
-                    // tests for eventual validation errors
-                    if(validation.error){
+
+                    if(validation.error && (validation.error.details[0].path[0] == 'uid' || validation.error.details[0].path[0] == 'conf')){
                         return res.status(400).send("valition error ");
                     }
+                    // tests for eventual validation errors
+                    if(validation.error){
+                        for(let i = 0; i < validation.error.details.length; i++){
+                            if(validation.error.details[i].path[1] != 'u' && validation.error.details[i].path[1] != 'd' && validation.error.details[i].path[1] != 't'){
+                                delete formated_data.m[`${validation.error.details[i].path[1]}`];
+                            }else{
+                                delete formated_data.m[`${validation.error.details[i].path[1]}`][`${validation.error.details[i].path[2]}`];
+                            }
+                            // objectError[validation.error.details[i].path[0]] = validation.error.details[i].path[0];
+                        }
+
+                        if(formated_data.m.hasOwnProperty('u') && Object.keys(formated_data.m.u).length === 0){
+                            delete formated_data.m.u;
+                        }
+
+                        if(formated_data.m.hasOwnProperty('d') && Object.keys(formated_data.m.d).length === 0){
+                            delete formated_data.m.d;
+                        }
+
+                        if(formated_data.m.hasOwnProperty('t') && Object.keys(formated_data.m.t).length === 0){
+                            delete formated_data.m.t;
+                        }  
+                        
+                        if(Object.keys(formated_data.m).length < 2){
+                            return res.status(400).send("m must have 2 fields");
+                        } 
+                    }
+
                     // tests for the existence of the br variable. If true it means new sessions
                     if(formated_data.m.br){
                         var newId = config.mongoose.Types.ObjectId();
@@ -53,11 +81,10 @@ var wrapper = function (config){
                             return res.status(400).send("invalid jmmc_objectId"); 
                         }
                     }
-                    }else{
-                        return res.status(400).send("must contains the m variable");
-                    }
+                }else{
+                    return res.status(400).send("must contains the m variable");
+                }
             }catch (error) {
-                console.log(error)
                 return res.status(500).send("something bad happened");
             }
         }else if( req.url === "/getClient" && req.method === 'GET'){
