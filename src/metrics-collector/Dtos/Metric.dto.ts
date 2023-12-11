f57a3df55b6ecc258d375bb65ff49d149722850d
youssefElMkhantar <youssef.mkht@gmail.com>
2023-12-11 20:19:21 +0100

    adding validation

--- src/metrics-collector/Dtos/Metric.dto.ts
+++ src/metrics-collector/Dtos/Metric.dto.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { IsArray, IsString } from 'class-validator';
 
 @Injectable()
-export class MetricDTO {}
+export class MetricDTO {
+  @IsString()
+  conf: string;
+
+  @IsString()
+  uid: string;
+
+  @IsArray()
+  m: any[];
+}
--- src/metrics-collector/metrics-collector.service.ts
+++ src/metrics-collector/metrics-collector.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Metric } from './schemas/Metric.schema';
-import { Model } from 'mongoose'
+import { Model } from 'mongoose';
 import { MetricDTO } from './Dtos/Metric.dto';
 
 @Injectable()
 export class MetricsCollectorService {
-    constructor(@InjectModel(Metric.name) private metricModel: Model<Metric>) {}
+  constructor(@InjectModel(Metric.name) private metricModel: Model<Metric>) {}
 
-    async create(body: MetricDTO) : Promise<Metric>{
-        const metric = new this.metricModel({conf: 'hhh', uid: 'haha', m: ['hhh', 'kjkjk', {metric: '46654'}]})
-        return metric.save()
-    }
+  async create(body: MetricDTO): Promise<Metric> {
+    const metric = new this.metricModel(body);
+    return metric.save();
+  }
 }
