75c94fe8379b2fa1910bdaa6cf82807f4b1f706a
Hamza KHAIT <HamzaKhait@users.noreply.github.com>
2024-03-25 14:58:05 +0100

    adding custom listening port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser(process.env.COOKIE_SECRET));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
